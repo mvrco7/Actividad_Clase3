38d231d2e8be9f6ea399407f6a074fd7e0fbafa7
mvrco7 <usuario@DESKTOP-L87TJJC>
2024-08-22 02:40:07 -0400

    Agregar archivos de proyecto.

---
+++ clase3/Form1.Designer.vb
@@ -0,0 +1,85 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txt1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblestado = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(111, 249)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Generar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txt1
+        '
+        Me.txt1.Location = New System.Drawing.Point(101, 212)
+        Me.txt1.Multiline = True
+        Me.txt1.Name = "txt1"
+        Me.txt1.Size = New System.Drawing.Size(100, 20)
+        Me.txt1.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(98, 186)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(109, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Escribe tu archivo .txt"
+        '
+        'lblestado
+        '
+        Me.lblestado.AutoSize = True
+        Me.lblestado.Location = New System.Drawing.Point(135, 304)
+        Me.lblestado.Name = "lblestado"
+        Me.lblestado.Size = New System.Drawing.Size(0, 13)
+        Me.lblestado.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(295, 450)
+        Me.Controls.Add(Me.lblestado)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txt1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblestado As Label
+End Class
---
+++ clase3/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rutaArchivo As String = "C:\Users\usuario\Desktop\trabajos\punto net.txt"
+        Try
+            If My.Computer.FileSystem.FileExists(rutaArchivo) Then
+                Dim contenido As String = My.Computer.FileSystem.ReadAllText(rutaArchivo)
+                txt1.Text = contenido
+                lblestado.Text = "Archivo cargado correctamente......."
+            Else
+                lblestado.Text = "El archivo no fue encontrado."
+            End If
+        Catch ex As Exception
+            lblestado.Text = "Error al cargar el archivo: " & ex.Message
+        End Try
+
+    End Sub
+End Class
